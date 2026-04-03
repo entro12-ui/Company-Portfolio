3315276f4e7062b7a27c3fd0e2b8a93b0ca66dae
natnaelmekuria98 <mekurianatnael98@gmail.com>
2026-04-03 15:43:11 +0300

    added Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-554N8BGQ');
+          `}
+        </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -56,6 +65,14 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${ubuntu.variable} ${hankenGrotesk.variable} font-sans`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-554N8BGQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <noscript>
           <img
             height="1"
